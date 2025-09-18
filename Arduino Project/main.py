b755f2c435ca9e7e0cc922e93265c6a69cfe212c
Ryan Dowd <rdowd2530@hotmail.com>
2025-09-18 19:50:14 -0400

    Got the data identifers working. Not printing all data for some reason ill fix tmr.

--- Arduino Project/main.py
+++ Arduino Project/main.py
@@ -1,5 +1,7 @@
 import re
 import time
+from idlelib.textview import ViewFrame
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -8,7 +10,7 @@ from selenium.webdriver.common.by import By
 
 print("Starting Scrap...")
 
-URL = "https://aviationweather.gov/api/data/metar?ids=KFDK%2CKIAD&hours=0&order=id%2C-obs&sep=true"
+URL = "https://aviationweather.gov/api/data/metar?ids=KFDK%2CKIAD%2CKBFR&hours=0&order=id%2C-obs&sep=true"
 driver = webdriver.Chrome()
 driver.get(URL)
 
@@ -25,8 +27,17 @@ def vis_sky(metar_text):
     chosen_sky = [s for s in skies if s.startswith(("BKN", "OVC"))]
 
     return vis, chosen_sky
+CATEGORIES = {
+    "VFR" : 1,
+    "MVFR" : 2,
+    "IFR" : 3,
+    "LIFR" : 4,
+    "NA" : 5
+}
 
 row_data = driver.find_element(By.XPATH, '/html/body/pre').text.strip()
+
+results = {}
 
 for line in row_data.splitlines():
     if not line.startswith(("METAR", "SPECI")):
@@ -34,16 +45,37 @@ for line in row_data.splitlines():
 
     vis, sky = vis_sky(line)
 
-    if vis and sky:
-        print(vis, " ".join(sky))
-    elif vis:
-        print(f"{vis} (no BKN or OVC")
-    elif sky:
-        print(" ".join(sky))
+
+    if vis:
+        if vis.startswith("10SM"):
+            category = "VFR"
+    elif vis.startswith(("5SM","4SM","3SM")):
+        category= "MVFR"
+    elif vis.startswith(("2SM", "1SM")):
+        category = "IFR"
+    elif vis.startswith("0SM"):
+        category = "LIFR"
     else:
-        print("No visibility or sky layer found:")
+        category = "NA"
+else:
+    category = "NA"
+
+    category_label = CATEGORIES[category]
+
+    station_id = line.split()[1]
+
+    results[station_id] = {
+        "visibility": vis if vis else "N/A",
+        "sky": sky if sky else ["N/A"],
+        "category_label": category,
+        "category_value": category_label
+    }
+
+    print (f"{station_id}: {vis or 'No Vis'} | Sky: {' '.join(sky) if sky else 'None'} | Category: {category} ({category_label})")
 
 driver.quit()
+
+
     
 
 
